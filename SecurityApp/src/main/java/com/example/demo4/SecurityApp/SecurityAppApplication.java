5659373e4e94733fd325c2073f7f03a88a2e00e3
Aryan <aryankiran28@gmail.com>
2025-12-22 12:31:49 +0530

    configured spring security

---
+++ SecurityApp/src/main/java/com/example/demo4/SecurityApp/SecurityAppApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo4.SecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAppApplication.class, args);
+	}
+
+}
+git init
+git add README.md
+git commit -m "configured spring security"
+git branch -M main
+git remote add origin https://github.com/AryanKiran/spring-security.git
+git push -u origin main
+
